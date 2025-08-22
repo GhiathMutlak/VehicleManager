04e5cd894b6700e81cb51acc367684a41ecba77d
Giyas MITLAK <giyas@mantasolutions.online>
2025-08-22 19:32:48 +0200

    Initial project setup.

---
+++ app/src/main/java/com/carly/vehiclemanager/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.carly.vehiclemanager
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.carly.vehiclemanager.ui.theme.VehicleManagerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        installSplashScreen().apply {
+            // Keep splash screen until the app is ready
+            // TODO: Replace with actual loading logic
+            setKeepOnScreenCondition { true }
+        }
+
+        enableEdgeToEdge()
+
+        setContent {
+            VehicleManagerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+
+                }
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/carly/vehiclemanager/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package com.carly.vehiclemanager.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val primary = Color(0xFFFB9B0A)
+val secondary = Color(0xFFFFD600)
+
+val fontDark = Color(0xFFC2CDDF)
+val fontLight = Color(0xFFF7F7F7)
+
+val backgroundDark = Color(0xFF23262B)
+val backgroundLight = Color(0xFF3B404A)
